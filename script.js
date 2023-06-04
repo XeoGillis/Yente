87616dba432ff5d5f81061982948ea22ecb78a43
Xeo Gillis <xeo.gillis@student.hogent.be>
2023-06-04 15:43:47 +0200

    init

---
+++ script.js
@@ -0,0 +1,17 @@
+function Valideer(id) {
+  var answer;
+  switch(id) {
+    case '0': answer = '26-06-2023'; break;
+    case '1': answer = 'puki'; break;
+    case '2': answer = 'corona'; break;
+    case '3': answer = 'gouden carolus'; break;
+    case '4': answer = 'karel de grote'; break;
+  }
+  if (document.getElementById(`question_${id}`).value.toLowerCase() === answer) {
+    document.getElementById(`answer_${id}_question`).style.visibility = 'visible';
+    var next = (parseInt(id)+1).toString();
+    document.getElementById(`question_${next}`).style.visibility = 'visible';
+    document.getElementById(`question_${next}_question`).style.visibility = 'visible';
+    document.getElementById(`button_${next}_question`).style.visibility = 'visible';
+  }
+}
